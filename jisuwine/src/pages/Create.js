0a1d648ef82ada3b46276773836bf80137ae2851
jisu yeo <jisuyeo@jisus-air.myfiosgateway.com>
2021-04-26 08:55:48 -0400

    not done

--- jisuwine/src/pages/Create.js
+++ jisuwine/src/pages/Create.js
@@ -1,4 +1,25 @@
-const Create = () => {
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
+const Create = (props) => {
+
+    const [newBook, setNewBook] = useState([])
+
+    const fetchNewbook = () =>{
+        axios.post('http://myapi-profstream.herokuapp.com/api/235b48/books',{
+            title:title,
+            author:author,
+            release_data:release_date,
+            image: image
+        }).then((response)=>{
+
+        })
+    }
+
+    }
+
+    
+    
     return(
         <div>
             hello from Create
