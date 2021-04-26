54bc1f61a2b0d1d2abea20d2316ee83aa23286ca
DiddySmooth <avery.mcclead@gmail.com>
2021-04-26 07:28:44 -0500

    grayson

--- wines/src/Components/Pages/Wine.js
+++ wines/src/Components/Pages/Wine.js
@@ -17,9 +17,35 @@ const Wine = () => {
     const deleteWine = async () => {
         let response = await axios.delete(`http://myapi-profstream.herokuapp.com/api/9d4029/wines/${id}`)
     }
-
-    const editWine = async () => {
-        
+    const editWine = (e) => {
+        console.log(e.target[0].value)
+       axios.pust('http://myapi-profstream.herokuapp.com/api/9d4029/wines',
+        {
+            name: e.target[0].value, 
+            year:e.target[1].value, 
+            grapes:e.target[2].value, 
+            country:e.target[3].value,
+            region:e.target[4].value,
+            description:e.target[5].value,
+            picture:e.target[6].value,
+            price:e.target[7].value
+            
+        })
+    }
+    const displayEdit = async (e) => {
+        return(
+            <form onSubmit={(e) => {e.preventDefault(); editWine(e)}}>
+                <input value="Name"/>
+                <input value="Year"/>
+                <input value="Grapes"/>
+                <input value="Country"/>
+                <input value="Region"/>
+                <input value="Description"/>
+                <input value="Picture"/>
+                <input value="Price"/>
+                <input type="submit" />
+            </form>
+        )
     }
 
     return(
@@ -32,8 +58,8 @@ const Wine = () => {
             <p>{wine.description}</p>
             <p>{wine.picture}</p>
             <p>{wine.price}</p>
-            <button onClick={deleteWine}>Delete</button>
-            <button onClick={editWine}>Edit</button>
+            <button onClick={ () => {deleteWine()}}>Delete</button>
+            <button onClick={ (e) => {displayEdit(e)}}>Edit</button>
         </div>
     )
 }
